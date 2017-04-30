7a6893fb4141545a4883737ba2ed554f5ac74577
Ferdinand Prantl <prantlf@gmail.com>
2017-04-30 16:07:17 +0200

    Include unit tests for file moving failures

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var os = require('os'),
-    path = require('path');
+    path = require('path'),
+    chalk = require('chalk');
 
 module.exports = function (grunt) {
 
@@ -83,6 +84,32 @@ module.exports = function (grunt) {
         },
         src: 'test/work/move_across_volumes/file',
         dest: path.join(tmp, 'grunt-move/file')
+      },
+      failed_empty: {
+      },
+      failed_missing: {
+        src: 'test/work/missing/old',
+        dest: 'test/work/missing/new'
+      },
+      failed_invalid_destination: {
+        src: 'test/work/failed_invalid_destination/file',
+        dest: 'test/work/failed_invalid_destination/:*?\\//file',
+      },
+      failed_move_across_volumes: {
+        src: 'test/work/failed_move_across_volumes/file',
+        dest: path.join(tmp, 'grunt-move/file')
+      }
+    },
+
+    'continue:check-warnings': {
+      test: {
+        warnings: [
+          'No files or directories specified.',
+          'No files or directories found at ' +
+              chalk.cyan('test/work/missing/old') + '.',
+          'Moving files across devices has not been enabled.',
+          'Moving failed.'
+        ]
       }
     },
 
@@ -135,6 +162,7 @@ module.exports = function (grunt) {
 
   grunt.loadTasks(coverage ? 'coverage/tasks' : 'tasks');
 
+  grunt.loadNpmTasks('grunt-continue');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-jshint');
@@ -142,9 +170,69 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-coveralls');
   grunt.loadNpmTasks('grunt-istanbul');
 
+  grunt.registerTask('continue:clear-warnings',
+      'Clears warnings remembered by grunt-continue.', function () {
+        grunt.config.set('grunt-continue:warnings', []);
+      });
+
+  grunt.registerTask('continue:check-any-warnings',
+      'Checks if any warnings were remembered by grunt-continue.', function () {
+        var actual = grunt.config('grunt-continue:warnings') || [];
+        grunt.verbose.writeln('Some warnings were expected.');
+        if (actual.length) {
+          actual.forEach(function (warning) {
+            grunt.verbose.writeln('Actual warning: "' + warning + '".');
+          });
+          grunt.log.ok(actual.length +
+              grunt.util.pluralize(actual.length, ' warning/ warnings') +
+              ' were found.');
+        } else {
+          grunt.fail.warn('No warnings found.');
+        }
+      });
+
+  grunt.registerMultiTask('continue:check-warnings',
+      'Checks warnings remembered by grunt-continue.', function () {
+        var expected = this.data.warnings,
+            actual = grunt.config('grunt-continue:warnings') || [];
+        if (!expected) {
+          grunt.fail.warn('No expected warnings specified.');
+        }
+        grunt.verbose.writeln(expected.length +
+            grunt.util.pluralize(expected.length, ' warning/ warnings') +
+            ' were expected.');
+        actual.forEach(function (warning) {
+          grunt.verbose.writeln('Actual warning: "' + warning + '".');
+          if (expected.indexOf(warning) < 0) {
+            grunt.fail.warn('Unexpected warning: "' + warning + '".');
+          }
+        });
+        expected.forEach(function (warning) {
+          if (actual.indexOf(warning) < 0) {
+            grunt.fail.warn('Warning not found: "' + warning + '".');
+          }
+        });
+        grunt.log.ok(actual.length +
+            grunt.util.pluralize(actual.length, ' warning/ warnings') +
+            ' were expected and found.');
+      });
+
   grunt.registerTask('default', coverage ?
-    ['jshint', 'clean', 'instrument', 'copy', 'move', 'nodeunit',
-     'storeCoverage', 'makeReport'] :
-    ['jshint', 'clean:tests', 'copy', 'move', 'nodeunit']);
+    ['jshint', 'clean', 'instrument', 'copy',
+     'move:empty', 'move:missing', 'move:rename', 'move:move_with_rename',
+     'move:move_without_rename', 'move:move_more', 'move:move_with_wildcard',
+     'move:move_with_wildcard_and_cwd', 'move:move_directory',
+     'move:move_across_volumes', 'continue:on', 'move:failed_empty',
+     'move:failed_missing', 'move:failed_invalid_destination',
+     'move:failed_move_across_volumes', 'continue:off',
+     'continue:check-warnings', 'nodeunit', 'storeCoverage', 'makeReport'] :
+    ['jshint', 'clean:tests', 'copy',
+     'move:empty', 'move:missing', 'move:rename', 'move:move_with_rename',
+     'move:move_without_rename', 'move:move_more', 'move:move_with_wildcard',
+     'move:move_with_wildcard_and_cwd', 'move:move_directory',
+     'move:move_across_volumes', 'continue:on', 'move:failed_empty',
+     'move:failed_missing', 'move:failed_invalid_destination',
+     'move:failed_move_across_volumes', 'continue:off',
+     'continue:check-warnings', 'nodeunit']);
 
 };
